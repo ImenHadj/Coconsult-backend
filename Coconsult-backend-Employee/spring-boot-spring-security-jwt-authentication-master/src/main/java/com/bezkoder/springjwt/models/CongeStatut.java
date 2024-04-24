9aa7191dd382b052a9c0de458167083be5f3e5fa
akacha-nacer <145552325+akacha-nacer@users.noreply.github.com>
2024-04-24 19:49:39 +0100

    Delete Coconsult-backend-Employee directory

--- Coconsult-backend-Employee/spring-boot-spring-security-jwt-authentication-master/src/main/java/com/bezkoder/springjwt/models/CongeStatut.java
+++
@@ -1,7 +0,0 @@
-package com.bezkoder.springjwt.models;
-
-public enum CongeStatut {
-    PENDING,
-    REJECTED,
-    VALIDATED,
-}
--- Coconsult-backend-Employee/spring-boot-spring-security-jwt-authentication-master/src/main/java/com/bezkoder/springjwt/repository/TeamRepo.java
+++
@@ -1,9 +0,0 @@
-package com.bezkoder.springjwt.repository;
-
-import com.bezkoder.springjwt.models.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamRepo extends JpaRepository<Team,Long> {
-
-
-}
