0183abcb2f3022a1e4d151e2d7271b25c03a4118
Anis FETOUI <anis.fetoui@esprit.tn>
2024-04-30 09:20:11 +0200

    all the work

--- spring-boot-spring-security-jwt-authentication-master/src/main/java/com/bezkoder/springjwt/Service/ServiceContratEmpl.java
+++ spring-boot-spring-security-jwt-authentication-master/src/main/java/com/bezkoder/springjwt/Service/ServiceContratEmpl.java
@@ -16,14 +16,12 @@ import com.lowagie.text.Font;
 import com.lowagie.text.pdf.PdfWriter;
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
 import javax.imageio.ImageIO;
 import com.lowagie.text.Image;
-import org.springframework.web.ErrorResponse;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -59,9 +57,6 @@ public class ServiceContratEmpl implements IServiceContratEmpl {
                 contrat.setEmpl(emp);
                 contratEmplRepo.save(contrat);
                 return ResponseEntity.ok(contrat.getId_contrat_e());
-
-
-
             } else {
                 Map<String, String> errorResponse = new HashMap<>();
 
@@ -76,10 +71,7 @@ public class ServiceContratEmpl implements IServiceContratEmpl {
         contrat.setEmpl(emp);
         contratEmplRepo.save(contrat);
         return ResponseEntity.ok(contrat.getId_contrat_e());
-
     }
-
-
     public boolean ContratEmployeeIsValid(ContratEmployee contrat, Employee em) {
         Set<ContratEmployee> oldContracts = em.getContratEmployees();
         int nb = 0;
@@ -96,37 +88,40 @@ public class ServiceContratEmpl implements IServiceContratEmpl {
         }
         return true;
     }
-
     @Override
-    public ResponseEntity<?> updateContratEmployee(ContratEmployee Updatedcontrat, Long id) {
-        ContratEmployee contratEmployee = contratEmplRepo.findById(id).get();
-        Employee emp = contratEmployee.getEmpl();
-        if (ContratEmployeeIsValid(Updatedcontrat, emp)) {
-            ContratEmployee updatedEmployee = ContratEmployee.builder()
-                    .rib(Updatedcontrat.getRib())
-                    .date_debut(Updatedcontrat.getDate_debut())
-                    .date_fin(Updatedcontrat.getDate_fin())
-                    .duree_hebdomadaire(Updatedcontrat.getDuree_hebdomadaire())
-                    .salaire_base(Updatedcontrat.getSalaire_base())
-                    .montant_heures_supplementaires(Updatedcontrat.getMontant_heures_supplementaires())
-                    .numeroSecuriteSociale(Updatedcontrat.getNumeroSecuriteSociale())
-                    .duree_hebdomadaire(Updatedcontrat.getDuree_hebdomadaire())
-                    .montant_Conge_Absence(Updatedcontrat.getMontant_Conge_Absence())
-                    .typeCE(Updatedcontrat.getTypeCE())
-                    .isArchive(Updatedcontrat.getIsArchive())
-                    .pourcentage(Updatedcontrat.getPourcentage())
-                    .build();
+    public ResponseEntity<?> updateContratEmployee(ContratEmployee updatedContrat, Long id) {
+        Optional<ContratEmployee> optionalContrat = contratEmplRepo.findById(id);
 
-            contratEmplRepo.save(updatedEmployee);
-            return ResponseEntity.ok(updatedEmployee.getId_contrat_e());
+        if (optionalContrat.isPresent()) {
+            ContratEmployee existingContrat = optionalContrat.get();
+            Employee emp = existingContrat.getEmpl();
+
+            if (ContratEmployeeIsValid(updatedContrat, emp)) {
+                existingContrat.setRib(updatedContrat.getRib());
+                existingContrat.setDate_debut(updatedContrat.getDate_debut());
+                existingContrat.setDate_fin(updatedContrat.getDate_fin());
+                existingContrat.setDuree_hebdomadaire(updatedContrat.getDuree_hebdomadaire());
+                existingContrat.setSalaire_base(updatedContrat.getSalaire_base());
+                existingContrat.setMontant_heures_supplementaires(updatedContrat.getMontant_heures_supplementaires());
+                existingContrat.setNumeroSecuriteSociale(updatedContrat.getNumeroSecuriteSociale());
+                existingContrat.setMontant_Conge_Absence(updatedContrat.getMontant_Conge_Absence());
+                existingContrat.setTypeCE(updatedContrat.getTypeCE());
+                existingContrat.setIsArchive(updatedContrat.getIsArchive());
+                existingContrat.setPourcentage(updatedContrat.getPourcentage());
+
+                ContratEmployee updatedContratEmployee = contratEmplRepo.save(existingContrat);
+                return ResponseEntity.ok(updatedContratEmployee.getId_contrat_e());
+            } else {
+                Map<String, String> errorResponse = new HashMap<>();
+                errorResponse.put("error", "Invalid Contrat request. Please check your inputs.");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+            }
         } else {
             Map<String, String> errorResponse = new HashMap<>();
-            errorResponse.put("error", "Invalid Contrat request. Please check your inputs.");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+            errorResponse.put("error", "Contrat not found with ID: " + id);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
         }
     }
-
-
     @Override
     public void deleteContratEmployee(Long id) {
         contratEmplRepo.deleteById(id);
@@ -163,9 +158,6 @@ public class ServiceContratEmpl implements IServiceContratEmpl {
     public Integer countByIsArchiveIsFalseAndDate_debutBetween(Date startDate, Date endDate) {
         return contratEmplRepo.countByIsArchiveIsFalseAndDateDebutBetween(startDate, endDate);
     }
-
-
-
 public void export(HttpServletResponse response, ContratEmployee contrat,Long id) throws IOException, WriterException, DocumentException {
     Document document = new Document(PageSize.A4);
     ByteArrayOutputStream qrCodeStream = new ByteArrayOutputStream();
@@ -176,26 +168,19 @@ public void export(HttpServletResponse response, ContratEmployee contrat,Long id
     Long userId =employee.getUserId();
     User user = userRepository.findById(userId).get();
 
-
-
-    // Generate QR code data from contract details
     String qrCodeData ="{\"UserName\":\"" + user.getUsername() + "{\"rib\":\"" + contrat.getRib() + "\",\"socialSecurityNumber\":\"" + contrat.getNumeroSecuriteSociale() + "\",\"startDate\":\"" + contrat.getDate_debut() + "\",\"endDate\":\"" + contrat.getDate_fin() + "\"}";
 
-    // Generate QR code image
     generateQRCodeImage(qrCodeData, qrCodeStream);
 
-    // Add QR code image to the PDF document
     Image qrCodeImage = Image.getInstance(qrCodeStream.toByteArray());
     qrCodeImage.scaleAbsolute(100, 100); // Adjust size as needed
     document.add(qrCodeImage);
 
-    // Adding contract details
     Font fontTitle = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
     fontTitle.setSize(18);
     Paragraph paragraphTitle = new Paragraph("Contract Details", fontTitle);
     paragraphTitle.setAlignment(Paragraph.ALIGN_CENTER);
     document.add(paragraphTitle);
-
     Font fontData = FontFactory.getFont(FontFactory.HELVETICA);
     fontData.setSize(12);
     Paragraph paragraphData = new Paragraph();
@@ -212,8 +197,6 @@ public void export(HttpServletResponse response, ContratEmployee contrat,Long id
     paragraphData.add("Percentage: " + contrat.getPourcentage() + " TD\n");
     paragraphData.add("Is Archived: " + contrat.getIsArchive() + "\n");
     document.add(paragraphData);
-
-
     document.close();
 }
 
